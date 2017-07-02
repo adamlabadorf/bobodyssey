cb2dc64167b5bde30ff16be1c3f687750acc86c4
Adam Labadorf <alabadorf@gmail.com>
2017-07-02 16:37:16 -0400

    tilemap and tileset udpate

--- bobodyssey/assets/tilemaps/platforms.tsx
+++ bobodyssey/assets/tilemaps/platforms.tsx
@@ -1,4 +1,7 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="platforms" tilewidth="32" tileheight="32" tilecount="10" columns="10">
- <image source="../sprites/level_0_platforms.png" width="320" height="32"/>
+<tileset name="platforms" tilewidth="320" tileheight="32" tilecount="1" columns="0">
+ <grid orientation="orthogonal" width="32" height="32"/>
+ <tile id="0">
+  <image width="320" height="32" source="../sprites/level_0_platforms.png"/>
+ </tile>
 </tileset>
