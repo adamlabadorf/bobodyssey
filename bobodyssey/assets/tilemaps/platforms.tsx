385e1cdee9442b73780cc53c4e68a930a1d73b69
Adam Labadorf <alabadorf@gmail.com>
2017-07-01 20:45:49 -0400

    preliminary level tilemap

---
+++ bobodyssey/assets/tilemaps/platforms.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="platforms" tilewidth="32" tileheight="32" tilecount="10" columns="10">
+ <image source="../sprites/level_0_platforms.png" width="320" height="32"/>
+</tileset>
